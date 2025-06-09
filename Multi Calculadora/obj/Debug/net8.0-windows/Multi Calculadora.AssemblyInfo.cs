987f81aa35b3fdb803e1fcd0590d1efaf700921a
Diego Silveira <diegocilveira@gmail.com>
2025-06-08 23:59:25 -0300

    layout multi calculadora

--- Multi Calculadora/obj/Debug/net8.0-windows/Multi Calculadora.AssemblyInfo.cs
+++ Multi Calculadora/obj/Debug/net8.0-windows/Multi Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Multi Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de04efa184282216acb8e8b71d4bd51c5d525f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e77d9a33e71ffdb8a5ba721f9f31bbf5ebe8a27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Multi Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Multi Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
